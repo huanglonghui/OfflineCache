a8fd7b5a9096ef434c8a73cb3eb7920d30eadd5a
黄龙辉 <ghuanglonghui@gmail.com>
2014-05-12 22:36:32 +0800

    Initial Commit

---
+++ OfflineCache/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OfflineCache
+//
+//  Created by 黄龙辉 on 14-5-12.
+//  Copyright (c) 2014年 黄龙辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
